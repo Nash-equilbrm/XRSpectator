5437ab89fb3fdb087fb511bc31ec49aaa7bf8b19
Nash-equilbrm <dohoainam17052001@gmail.com>
2023-07-10 14:44:47 +0700

    +add an info panel when displaying monster models

--- Assets/Scripts/Multiplayer/NetworkManager.cs
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -81,6 +81,8 @@ public partial class GameManager
 
     public override void OnJoinedRoom()
     {
+        UnityEngine.XR.XRSettings.renderViewportScale = 0.7f;
+
         if (isAudience)
         {
             InitZED();
